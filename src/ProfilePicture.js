0b97d88501c28214596fbbdba3ac42f511c79640
malte18 <malte.sielski@icloud.com>
2019-02-23 20:34:46 +0100

    stateless component for ppic rendering

--- src/ProfilePicture.js
+++ src/ProfilePicture.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Image from "react-bootstrap/Image"
 
-export const ProfilePicture = (props) => {
-    var pictureUrl = props.webId.replace("card#me", "aa.jpeg")
 
-    return (
-        <Image src={pictureUrl} fluid/>
-    )
-}
+export const ProfilePicture = (props) => {
+        return (
+            <Image src={props.picture} fluid/>
+        )
+    }
+
