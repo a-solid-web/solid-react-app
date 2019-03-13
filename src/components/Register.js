d29590426058f8406f0bbc9c4450ca479723e34c
malte18 <malte.sielski@icloud.com>
2019-03-13 12:04:50 +0100

    register works

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,26 +3,59 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import { createHash } from "crypto";
 
-const Register = (props) => {
-    return (
-        <Container>
-            <Row style={{marginTop: "5%"}}>
-                <Form method="POST" action="https://solid.community/api/accounts/new">
-                    <Form.Label>Enter username</Form.Label>
-                    <Form.Control type="text" name="username"/>
-                    <Form.Label>Enter password</Form.Label>
-                    <Form.Control type="text" name="password"/>
-                    <Form.Label>Enter name</Form.Label>
-                    <Form.Control type="text" name="name"/>
-                    <Form.Label>Enter email</Form.Label>
-                    <Form.Control type="text" name="email"/>
-                    <Button type="submit" style={{marginTop: "5%"}}>Register</Button>
-                </Form>
-            </Row>
-        </Container>
-    );
-};
+class Register extends React.Component {
+    
+    registerUser () {
+
+        var xhr = new XMLHttpRequest();
+        var url = "https://solid.community/api/accounts/new"
+
+        xhr.onreadystatechange = () => {
+            if( xhr.response == XMLHttpRequest.DONE) {
+                if(xhr.status == 403) {
+                    console.log("account already exists");
+                } else {
+                    console.log("account successfully created, LOGIN now!")
+                }
+            }
+        }
+
+        xhr.open("POST", url);
+        xhr.withCredentials = true;
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        var username = document.getElementById("username").value
+        var password = document.getElementById("password").value
+        var repeatPassword = document.getElementById("repeat-password").value
+        var name = document.getElementById("name").value
+        var email = document.getElementById("email").value
+        
+        xhr.send("username="+username+"&password="+password+"&repeat_password="+repeatPassword+"&name="+name+"&email="+email) 
+    }
+    render () {
+        return (
+            <Container>
+                <Row style={{marginTop: "5%"}}>
+                    <Form>
+                        <Form.Label>Enter username</Form.Label>
+                        <Form.Control id="username" type="text" name="username"/>
+                        <Form.Label>Enter password</Form.Label>
+                        <Form.Control id="password" type="password" name="password"/>
+                        <Form.Label>Repeat Password</Form.Label>
+                        <Form.Control id="repeat-password" type="password" name="repeat_password"/>
+                        <Form.Label>Enter name</Form.Label>
+                        <Form.Control id="name" type="text" name="name"/>
+                        <Form.Label>Enter email</Form.Label>
+                        <Form.Control id="email" type="text" name="email"/>
+                        {/* <Form.Control type="hidden" name="redirect_uri" value/> */}
+                        <Button style={{marginTop: "5%"}} onClick={this.registerUser.bind(this)}>Register</Button>
+                    </Form>
+                </Row>
+            </Container>
+        );
+    };
+}
 
 export default Register; 
 
